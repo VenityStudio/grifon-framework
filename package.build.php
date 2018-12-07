857e187715e4ab54aaeed41547fad6bfc0c06ebe
MWGuy <yousan4ik@gmail.com>
2018-12-07 19:01:55 +0300

    Fix build

--- package.build.php
+++ package.build.php
@@ -5,11 +5,10 @@ use packager\cli\Console;
 
 function task_build(Event $e)
 {
-    foreach ($e->package()->getAny("modules") as $module)
+    foreach ($e->package()->getAny("modules") as $module) {
         Tasks::runExternal('./grifon-' . $module, 'install', [], "yes");
-
-    foreach ($e->package()->getAny("modules") as $module)
     	Tasks::runExternal('./grifon-' . $module, 'publish', [], "yes");
+    }
 }
 
 function task_test(Event $e)
